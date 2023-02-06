318eb83bb265772ddc48d18778db2e9d81a20f72
Dobby <apple@Dobbyde-MacBook-Pro.local>
2023-02-06 10:53:16 -0500

    add initial code in server/connection/and db

---
+++ server.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+
+const routes = require("./controllers/index");
+const helpers = require("./utils/helpers");
+
+const Sequelize = require("./config/connection");
+const sequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: "secret",
+  cookie: {
+    maxAge: 3600000,
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new sequelizeStore({
+    db: Sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+Sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
