68e4a4eb39f37aa5c54bc455d6fb28afc218a274
Dobby <apple@Dobbyde-MacBook-Pro.local>
2023-02-08 11:30:36 -0500

    edit views

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const session = require("express-session");
 
-const routes = require("./controllers/index");
+const routes = require("./controllers/");
 const helpers = require("./utils/helpers");
 
 const Sequelize = require("./config/connection");
